cecc5c3ad3e0ca4835986bbdba937c4eece1a5f5
Pedrou <pdro4_4_6@hotmail.com>
2019-09-09 02:07:48 -0500

    Revert "Agregar archivos de proyecto."
    This reverts commit 233bb6b276126d60a6e158c491b462245d741975.

--- BTX.SERVICES/Enums/Logger/LoggerEnums.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BTX.SERVICES.Enums.Logger
-{
-    public static class LoggerEnums
-    {
-        public enum OutPutFileType { Text, Console, Database }
-        public enum MessageType { Message, Error, Warning }
-    }
-}
--- BTX.SERVICES/Services/Logger/ILoggerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using static BTX.SERVICES.Enums.Logger.LoggerEnums;
-
-namespace BTX.SERVICES.Services.Logger
-{
-    public interface ILoggerService
-    {
-        void LogMessage(string message, MessageType mType);
-    }
-}
