233bb6b276126d60a6e158c491b462245d741975
Pedrou <pdro4_4_6@hotmail.com>
2019-09-09 01:56:01 -0500

    Agregar archivos de proyecto.

---
+++ BTX.SERVICES/Enums/Logger/LoggerEnums.cs
@@ -0,0 +1,8 @@
+﻿namespace BTX.SERVICES.Enums.Logger
+{
+    public static class LoggerEnums
+    {
+        public enum OutPutFileType { Text, Console, Database }
+        public enum MessageType { Message, Error, Warning }
+    }
+}
---
+++ BTX.SERVICES/Helpers/Logger/LoggerHelpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using static BTX.SERVICES.Enums.Logger.LoggerEnums;
+
+namespace BTX.SERVICES.Helpers.Logger
+{
+    public static class LoggerHelpers
+    {
+        public static class Paths
+        {
+            public static string OutPutTextFilePath = $"{ConfigurationManager.AppSettings["LogFileDirectory"]}/LogFile_{DateTime.Now.ToString("dd_MM_yyyy")}.txt";
+        }
+
+        public static class Methods
+        {
+            public static void LogToDatabase(string message, MessageType type)
+            {
+                try
+                {
+                    var connection = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+                    connection.Open();
+
+                    var command = new SqlCommand("Insert into Log Values('" + message + "', " + type.ToString() + ")");
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+
+            public static void LogToFileText(string message, MessageType type)
+            {
+                if (!File.Exists(Paths.OutPutTextFilePath)) //~~~~~~ se creo una variable estática para la ruta de el archivo de salida, además la validación provoca un error puesto que lee una ruta que no existe.
+                    throw new Exception("Invalid output path file.");
+
+                string l = File.ReadAllText(Paths.OutPutTextFilePath);
+                l += $"{DateTime.Now.ToShortDateString()} - type: {type.ToString()} - message: {message}";
+
+                File.WriteAllText(Paths.OutPutTextFilePath, l);
+            }
+
+            public static void LogToConsole(string message, MessageType type)
+            {
+                switch (type)
+                {
+                    case MessageType.Message:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case MessageType.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case MessageType.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    default: break;
+                }
+
+                Console.WriteLine($"{DateTime.Now.ToShortDateString()} {message}");
+            }
+        }
+
+
+    }
+}
